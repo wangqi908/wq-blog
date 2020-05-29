7241c089f53c676da40c49f1419d677a7b1a3d8c
wangqi <wangqi908@hotmail.com>
2020-05-29 14:23:24 +0800

    封装富文本编辑器

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,7 @@ export { default as Foot } from './foot'
 export { default as Person } from './person'
 export { default as Recent } from './recent'
 export { default as ArticleList } from './articleList'
+export const WangEdit = () => import(/* webpackChunkName: "WangEdit" */ './WangEdit')
 
 import Card from './Card'
 
--- src/views/edit/index.vue
+++ src/views/edit/index.vue
@@ -1,53 +1,29 @@
 <template>
-  <div id="wangeditor">
-    <div ref="editorElem" style="text-align:left;"></div>
+  <div class="edit-page">
+    <WangEdit v-model="val" />
   </div>
 </template>
 
 <script>
-import E from 'wangeditor'
+import { WangEdit } from '@c'
 export default {
   name: 'Edit',
+  components: { WangEdit },
   data() {
     return {
-      editor: null,
-      menus: [
-        'head', // 标题
-        'bold', // 粗体
-        'fontSize', // 字号
-        'fontName', // 字体
-        'italic', // 斜体
-        'underline', // 下划线
-        'strikeThrough', // 删除线
-        'foreColor', // 文字颜色
-        'backColor', // 背景颜色
-        'link', // 插入链接
-        'list', // 列表
-        'justify', // 对齐方式
-        'quote', // 引用
-        'emoticon', // 表情
-        'image', // 插入图片
-        'table', // 表格
-        'video', // 插入视频
-        'code', // 插入代码
-        'undo', // 撤销
-        'redo' // 重复
-      ],
-      editorContent: ''
+      val: ''
     }
   },
-  mounted() {
-    let editor = new E(this.$refs.editorElem)
-    // 编辑器的事件，每次改变会获取其html内容
-    editor.customConfig.onchange = html => {
-      console.log(html)
+  watch: {
+    val(val) {
+      console.log(val)
     }
-
-    editor.customConfig.menus = this.menus
-
-    editor.customConfig.uploadImgShowBase64 = true
-
-    editor.create() // 创建富文本实例
   }
 }
 </script>
+
+<style scoped>
+.edit-page {
+  background-color: #fff;
+}
+</style>
