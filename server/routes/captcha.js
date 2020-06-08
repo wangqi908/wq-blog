493eb8dabc8cebcf6fb5d744b9de5577eadcca34
wangqi <wangqi908@hotmail.com>
2020-06-08 11:13:18 +0800

    登录验证码

--- server/routes/captcha.js
+++ server/routes/captcha.js
@@ -4,12 +4,14 @@ const router = express.Router()
 // https://www.npmjs.com/package/svg-captcha
 
 router.get('/', (req, res, next) => {
-  const captcha = svgCaptcha.create({
-    width: 90,
-    height: 35,
+  const captcha = svgCaptcha.createMathExpr({
+    width: 95,
+    height: 40,
     ignoreChars: false,
+    noise: 0,
     color: true
   })
+  console.log(captcha.text)
   req.session.captchaValue = captcha.text.toLowerCase()
 
   res.send({ code: 200, data: captcha.data })
